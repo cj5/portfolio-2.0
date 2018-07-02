894b5ad2f9d58a231c4ee1249c2de3f2a992a6dd
Chris <stac1221@gmail.com>
2018-07-01 23:13:24 -0500

    removed/added projects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
     let alignMobProjCap = ((vpW - (projImgW - 2)) / 2) - 26;
     $('.mobile-project-caption').css('width', projImgW);
 
-    $('.wrapper').css('display', 'none');    
+    $('.wrapper').css('display', 'none');
   }
 
   $(window).resize(function() {
@@ -71,119 +71,119 @@ $(document).ready(function(){
 });
 
 // ////////////////////
-// // cellular automata
+// // cellular automaton
 
-// let randomBinary = () => {
-//   return Math.round(Math.random())
-// }
-// randomBinary()
+let randomBinary = () => {
+  return Math.round(Math.random())
+}
+randomBinary()
 
-// const row = document.querySelector('.row')
+const row = document.querySelector('.row')
 
-// //creates first row with random class true or false
-// for (let i = 0; i <= 200; i++) {
-//   let div = document.createElement('div')
-//   document.querySelector('.row').appendChild(div)
-//   div.classList.add(randomBinary() ? 'true' : 'false')
-//   console.log('row width: ', row.offsetWidth)
-//   console.log('window width: ', window.innerWidth)
-//   if (row.offsetWidth >= window.innerWidth - 10) {
-//     break
-//   }
-// }
+//creates first row with random class true or false
+for (let i = 0; i <= 200; i++) {
+  let div = document.createElement('div')
+  document.querySelector('.row').appendChild(div)
+  div.classList.add(randomBinary() ? 'true' : 'false')
+  console.log('row width: ', row.offsetWidth)
+  console.log('window width: ', window.innerWidth)
+  if (row.offsetWidth >= window.innerWidth - 10) {
+    break
+  }
+}
 
-// const duplicateRow = () => {
-//   let allRows = document.querySelectorAll('.row')
-//   let lastRow = allRows[allRows.length - 1]
-//   let cloneLastRow = lastRow.cloneNode(true)
-//   document.querySelector('.wrapper').appendChild(cloneLastRow)
+const duplicateRow = () => {
+  let allRows = document.querySelectorAll('.row')
+  let lastRow = allRows[allRows.length - 1]
+  let cloneLastRow = lastRow.cloneNode(true)
+  document.querySelector('.wrapper').appendChild(cloneLastRow)
 
-//   automatizeRow(lastRow, cloneLastRow)
+  automatizeRow(lastRow, cloneLastRow)
 
-//   let wrapperHt = document.querySelector('.wrapper').offsetHeight
-//   // console.log('wrapperHt: ', wrapperHt)
-//   if (wrapperHt >= 500) {
-//     clearInterval(duplicateInterval)
-//   }
-// }
-// let duplicateInterval = setInterval(duplicateRow, 100)
+  let wrapperHt = document.querySelector('.wrapper').offsetHeight
+  // console.log('wrapperHt: ', wrapperHt)
+  if (wrapperHt >= 500) {
+    clearInterval(duplicateInterval)
+  }
+}
+let duplicateInterval = setInterval(duplicateRow, 100)
 
-// function automatizeRow(prevRow, newRow) {
-//   for (let i = 0; i < prevRow.childNodes.length; i++) {
-//     let newDiv = newRow.childNodes[i]
-//     // console.log('—— newDiv_' + `${i}`, newDiv)
-//     let prevMid = prevRow.childNodes[i]
-//     let prevLeft = prevRow.childNodes[i - 1] || prevRow.childNodes[prevRow.childNodes.length - 1]
-//     let prevRight = prevRow.childNodes[i + 1] || prevRow.childNodes[0]
-//     // console.log('prevLeft', prevLeft)
-//     // console.log('prevMid', prevMid)
-//     // console.log('prevRight', prevRight)
+function automatizeRow(prevRow, newRow) {
+  for (let i = 0; i < prevRow.childNodes.length; i++) {
+    let newDiv = newRow.childNodes[i]
+    // console.log('—— newDiv_' + `${i}`, newDiv)
+    let prevMid = prevRow.childNodes[i]
+    let prevLeft = prevRow.childNodes[i - 1] || prevRow.childNodes[prevRow.childNodes.length - 1]
+    let prevRight = prevRow.childNodes[i + 1] || prevRow.childNodes[0]
+    // console.log('prevLeft', prevLeft)
+    // console.log('prevMid', prevMid)
+    // console.log('prevRight', prevRight)
 
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//     // RULE 150
-//     if (prevLeft.classList.contains('true') &&
-//       prevMid.classList.contains('true') &&
-//       prevRight.classList.contains('true')
-//     ) {
-//       newDiv.classList.add('true')
-//       newDiv.classList.remove('false')
-//     }
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//     if (prevLeft.classList.contains('true') &&
-//       prevMid.classList.contains('true') &&
-//       prevRight.classList.contains('false')
-//     ) {
-//       newDiv.classList.add('false')
-//       newDiv.classList.remove('true')
-//     }
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//     if (prevLeft.classList.contains('true') &&
-//       prevMid.classList.contains('false') &&
-//       prevRight.classList.contains('true')
-//     ) {
-//       newDiv.classList.add('false')
-//       newDiv.classList.remove('true')
-//     }
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//     if (prevLeft.classList.contains('true') &&
-//       prevMid.classList.contains('false') &&
-//       prevRight.classList.contains('false')
-//     ) {
-//       newDiv.classList.add('true')
-//       newDiv.classList.remove('false')
-//     }
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//     if (prevLeft.classList.contains('false') &&
-//       prevMid.classList.contains('true') &&
-//       prevRight.classList.contains('true')
-//     ) {
-//       newDiv.classList.add('false')
-//       newDiv.classList.remove('true')
-//     }
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//     if (prevLeft.classList.contains('false') &&
-//       prevMid.classList.contains('true') &&
-//       prevRight.classList.contains('false')
-//     ) {
-//       newDiv.classList.add('true')
-//       newDiv.classList.remove('false')
-//     }
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//     if (prevLeft.classList.contains('false') &&
-//       prevMid.classList.contains('false') &&
-//       prevRight.classList.contains('true')
-//     ) {
-//       newDiv.classList.add('true')
-//       newDiv.classList.remove('false')
-//     }
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//     if (prevLeft.classList.contains('false') &&
-//       prevMid.classList.contains('false') &&
-//       prevRight.classList.contains('false')
-//     ) {
-//       newDiv.classList.add('false')
-//       newDiv.classList.remove('true')
-//     }
-//     // ~~~~~~~~~~~~~~~~~~~~~~~
-//   }
-// }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    // RULE 150
+    if (prevLeft.classList.contains('true') &&
+      prevMid.classList.contains('true') &&
+      prevRight.classList.contains('true')
+    ) {
+      newDiv.classList.add('true')
+      newDiv.classList.remove('false')
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    if (prevLeft.classList.contains('true') &&
+      prevMid.classList.contains('true') &&
+      prevRight.classList.contains('false')
+    ) {
+      newDiv.classList.add('false')
+      newDiv.classList.remove('true')
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    if (prevLeft.classList.contains('true') &&
+      prevMid.classList.contains('false') &&
+      prevRight.classList.contains('true')
+    ) {
+      newDiv.classList.add('false')
+      newDiv.classList.remove('true')
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    if (prevLeft.classList.contains('true') &&
+      prevMid.classList.contains('false') &&
+      prevRight.classList.contains('false')
+    ) {
+      newDiv.classList.add('true')
+      newDiv.classList.remove('false')
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    if (prevLeft.classList.contains('false') &&
+      prevMid.classList.contains('true') &&
+      prevRight.classList.contains('true')
+    ) {
+      newDiv.classList.add('false')
+      newDiv.classList.remove('true')
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    if (prevLeft.classList.contains('false') &&
+      prevMid.classList.contains('true') &&
+      prevRight.classList.contains('false')
+    ) {
+      newDiv.classList.add('true')
+      newDiv.classList.remove('false')
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    if (prevLeft.classList.contains('false') &&
+      prevMid.classList.contains('false') &&
+      prevRight.classList.contains('true')
+    ) {
+      newDiv.classList.add('true')
+      newDiv.classList.remove('false')
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    if (prevLeft.classList.contains('false') &&
+      prevMid.classList.contains('false') &&
+      prevRight.classList.contains('false')
+    ) {
+      newDiv.classList.add('false')
+      newDiv.classList.remove('true')
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+  }
+}
